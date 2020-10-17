ee0eef9ac6a9eadca974eef1c518bb920a14b75d
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-10-16 23:52:07 -0300

    Adds map marker

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
@@ -15,7 +16,28 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
-      />
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -22.9142946,
+            longitude: -43.2353886,
+          }}
+        >
+          <Callout
+            tooltip
+            onPress={() => {}}
+          >
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -28,5 +50,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 168,
+    height: 46,
+    paddingHorizontal: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
