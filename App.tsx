901162a53a4c43afd8d96837602c830c9aa978b3
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-10-17 00:06:08 -0300

    Adds fonts

--- App.tsx
+++ App.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    nunito700: Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -74,6 +86,7 @@ const styles = StyleSheet.create({
   },
 
   calloutText: {
+    fontFamily: 'nunito700',
     color: '#0089a5',
     fontSize: 14,
   },
@@ -97,6 +110,7 @@ const styles = StyleSheet.create({
   },
 
   footerText: {
+    fontFamily: 'nunito700',
     color: '#8fa7b3',
   },
 
